8b83e6c61da2137c0003e9bd9bc2fab5c0031ff7
rKremser03 <robin.kremser03@htl-salzburg.ac.at>
2021-05-17 13:19:32 +0200

    first 4 arrays

---
+++ Assets/MyGame/Scripts/PracticeArrays.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeArrays : MonoBehaviour
+{
+
+    public int[] array1 = new int[4];
+    public int[] array2 = new int[4] { 21, 22, 23, 24 };
+    public int[] array3 = { 31, 32, 33, 34, 35, 36, 37 };
+    public string[] array4 = new string[10];
+    public GameObject[] arrayColoredImageRed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        for (int i = 0; i < array4.Length; i++)
+        {
+            array4[i] = "array4";
+        }
+
+        arrayColoredImageRed = GameObject.FindGameObjectsWithTag("RedImage");
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
