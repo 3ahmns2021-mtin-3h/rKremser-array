5c5a5ffe0e3d7c2eb88d0f3c9807fc961d1f9aa7
rKremser03 <robin.kremser03@htl-salzburg.ac.at>
2021-05-31 13:14:02 +0200

    Aufgabe 3

--- Assets/MyGame/Scripts/PracticeArrays.cs
+++ Assets/MyGame/Scripts/PracticeArrays.cs
@@ -9,7 +9,7 @@ public class PracticeArrays : MonoBehaviour
     public int[] array2 = new int[4] { 21, 22, 23, 24 };
     public int[] array3 = { 31, 32, 33, 34, 35, 36, 37 };
     public string[] array4 = new string[10];
-    public GameObject[] arrayColoredImageRed;
+    public GameObject[] myRedImages;
 
     // Start is called before the first frame update
     void Start()
@@ -21,19 +21,26 @@ public class PracticeArrays : MonoBehaviour
             array4[i] = "array4";
         }
 
-        arrayColoredImageRed = GameObject.FindGameObjectsWithTag("RedImage");
+        myRedImages = GameObject.FindGameObjectsWithTag("RedImage");
+
+        Debug.Log(myRedImages.Length);
+
+        foreach(GameObject elem in myRedImages)
+        {
+            Debug.Log(elem.name);
+        }
 
         Debug.Log("Element 0 von array1 hat den Wert: " + array1[0]);
         Debug.Log("Element 0 von array2 hat den Wert: " + array2[0]);
         Debug.Log("Element 0 von array3 hat den Wert: " + array3[0]);
         Debug.Log("Element 0 von array4 hat den Wert: " + array4[0]);
-        Debug.Log("Name des ersten GameObjects: " + arrayColoredImageRed[0].name);
+        Debug.Log("Name des ersten GameObjects: " + myRedImages[0].name);
 
         Debug.Log("Das letzte Element von array1 hat den Wert: " + array1[array1.Length - 1]);
         Debug.Log("Das letzte Element von array2 hat den Wert: " + array2[array2.Length - 1]);
         Debug.Log("Das letzte Element von array3 hat den Wert: " + array3[array3.Length - 1]);
         Debug.Log("Das letzte Element von array4 hat den Wert: " + array4[array4.Length - 1]);
-        Debug.Log("Name des letzten GameObjects: " + arrayColoredImageRed[arrayColoredImageRed.Length -1].name);
+        Debug.Log("Name des letzten GameObjects: " + myRedImages[myRedImages.Length -1].name);
 
         PrintElements(array4);
     }
